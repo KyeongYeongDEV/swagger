23f475787718ca7c340980ab58e642a1cf29237e
KyeongYeongDEV <yeong7098@naver.com>
2024-10-07 15:53:11 +0900

    init

---
+++ server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import swaggerUi from 'swagger-ui-express';
+const swaggerDocument = require("./swagger-output.json");
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.get('/', (req, res) => {
+    res.send("main")
+})
+
+app.listen(port, () => {
+    console.log(`Port= ${port}, Server start`);
+}) 
